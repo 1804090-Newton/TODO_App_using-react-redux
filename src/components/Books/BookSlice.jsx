d582657afc3149dfe13ae41d1a884632f01393e8
Newton <u1804090@student.cuet.ac.bd>
2024-03-21 09:28:57 +0600

    read book using useSelector

--- src/components/Books/BookSlice.jsx
+++ src/components/Books/BookSlice.jsx
@@ -1,26 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
 
- const  {createSlice}=require('@reduxjs/toolkit');
+const initialBooks = {
+  books: [
+    { id: 1, title: "Beautiful Life", author: "Sourav vai" },
+    { id: 2, title: "redux er hate khori", author: "Newton" },
+  ],
+};
 
-   const initialBooks={
-    books:[
-        {id:1, title:"Beautiful Life" , author:" Sourav vai"},
-        {id:2, title:"redux er hate khori" , author:" Newton"},
+export const bookSlice = createSlice({
+  name: "books",
+  initialState: initialBooks,
+  reducers: {
+    showBooks: (state) => state,
+    addBook: (state,action)=>{
+      state.books.push(action.payload)
+    }
+  },
+});
 
-    ],
-   }
-
-
-   export const bookSlice=createSlice({
-     name:"books",
-     initialState: initialBooks,
-     reducers:{
-        showBooks:(state)=>state,
-     },
-   });
-
-   export const {showBooks}=bookSlice.actions;
-   export default BookSlice.reducer;
-
-
-
-
+export const { showBooks, addBook } = bookSlice.actions;
+export default bookSlice.reducer; 
--- src/components/Books/BooksView.jsx
+++ src/components/Books/BooksView.jsx
@@ -1,9 +1,39 @@
- 
 import React from 'react';
-const BooksView=()=>{
-    return(
+import { useSelector } from 'react-redux';
+
+const BooksView = () => {
+   
+    const books = useSelector((state) => state.booksReducer.books);
+
+    return (
         <div>
             <h2>List of Books</h2>
+            <table>
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Title</th>
+                        <th>Author</th> 
+                        <th>Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {books && books.map((book) => { 
+                        const { id, title, author } = book;
+                        return (
+                            <tr key={id}>
+                                <td>{id}</td>
+                                <td>{title}</td>
+                                <td>{author}</td>
+                                <td>
+                                    <button>Edit</button>
+                                    <button>Delete</button>
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
         </div>
     );
 };
